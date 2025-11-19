74b636ec9758c86a483406cbf1965d3b48d6ac8e
Bilal Hussien <bh255890@gmail.com>
2025-11-19 23:01:53 +0200

    edit serverless 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,49 @@
-import app from "../config/database";
+import express from "express";
+import serverless from "serverless-http";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
 import dbConnection from "../config/database";
+import globalErrorHandler from "../middleware/globalError";
+import { quizRouter } from "../routes/quiz/quiz";
+import { announcementRouter } from "../routes/announcement/announcement";
 
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json({ limit: "10kb" }));
+app.use(helmet());
+//app.use(mongoSanitize());
+
+app.use((req, res, next) => {
+  console.table({ path: req.url, method: req.method });
+  next();
+});
+
+// Routes
+app.use("/quiz", quizRouter);
+app.use("/announcement", announcementRouter);
+
+// Error handler
+app.use(globalErrorHandler);
+
+// Connect DB once
 let isConnected = false;
-
-async function connectDB() {
+const connectDBOnce = async () => {
   if (!isConnected) {
     await dbConnection();
     isConnected = true;
   }
-}
+};
 
-export default async function handler(req, res) {
-  await connectDB();
-  console.log("hello from vercl");
+// Export serverless handler
+const handler = async (req, res) => {
+  await connectDBOnce();
   return app(req, res);
-}
+};
+
+export default serverless(handler);
